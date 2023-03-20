0191b1e4b1de48b6805193b02bb4e3f135307bfe
rmafat <remimafat@gmail.com>
2023-03-20 17:24:50 +0100

    exo: lifecycle

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
 import './App.css';
+import { collection, CollectionReference, getDocs } from 'firebase/firestore';
 import NotesList from 'src/components/note/NotesList';
 import OpenModalButton from 'src/components/OpenModalButton';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Note } from 'src/types/NoteType';
+import { db } from 'src/config/firebase';
 
 const App = () => {
   const [notes, setNotes] = useState<Note[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const getNotes = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, 'notes') as CollectionReference<Note>);
+
+      const docs = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setNotes(docs);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    // Avec une fonction asynchrone
+    getNotes();
+
+    // Avec un then/catch
+
+    // getDocs(collection(db, 'notes') as CollectionReference<Note>)
+    //   .then((querySnapshot) => {
+    //     const docs = querySnapshot.docs.map((doc) => ({
+    //       id: doc.id,
+    //       ...doc.data(),
+    //     }));
+    //
+    //     setNotes(docs);
+    //   })
+    //   .catch((error) => console.error(error))
+    //   .finally(() => setLoading(false));
+  }, []);
 
   const addNote = (note: Note) => {
     setNotes([...notes, note]);
@@ -15,7 +54,9 @@ const App = () => {
     <div className="container mx-auto">
       <OpenModalButton addNote={addNote} />
 
-      <NotesList notes={notes} />
+      {loading
+        ? <div>Chargement...</div>
+        : <NotesList notes={notes} />}
     </div>
   );
 };
